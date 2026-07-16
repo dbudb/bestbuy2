0bab812959cc9eb3a51a321e398f27591efed06b
Dennis Budek <dennis.budek@protonmail.com>
2026-07-16 12:26:00 +0200

    init store class

--- main.py
+++ main.py
@@ -12,3 +12,15 @@ mac.show()
 
 bose.set_quantity(1000)
 bose.show()
+
+
+product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
+                products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                products.Product("Google Pixel 7", price=500, quantity=250),
+               ]
+
+best_buy = Store(product_list)
+products = best_buy.get_all_products()
+print(best_buy.get_total_quantity())
+print(best_buy.order([(products[0], 1), (products[1], 2)]))
+Put it in your main function in the same file, and make sure you are handling the importing in the right manner.
---
+++ store.py
@@ -0,0 +1,35 @@
+from typing import List
+from products import Product
+
+
+class Store:
+
+    def __init__(self, products: List[Product]):
+        self.products = products
+
+    def add_product(self, product):
+        """Adds a product to store"""
+        self.products.append(product)
+
+    def remove_product(self, product):
+        """Removes a product from store."""
+        self.products.remove(product)
+
+    def get_total_quantity(self) -> int:
+        """Returns how many items are in the store in total."""
+        return sum(product.get_quantity() for product in self.products)
+
+    def get_all_products(self) -> List[Product]:
+        """Returns all products in the store that are active."""
+        return [product for product in self.products if product.is_active()]
+
+    def order(self, shopping_list) -> float:
+        """Gets a list of tuples, where each tuple has 2 items:
+        Product (Product class) and quantity (int).
+        Buys the products and returns the total price of the order."""
+        total = 0
+
+        for product, quantity in shopping_list:
+            total += product.buy(quantity)
+
+        return total
