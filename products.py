f5105c8b62853dc7d7f272e1960c61568cf73b2e
Dennis Budek <dennis.budek@protonmail.com>
2026-07-16 10:45:12 +0200

    comments what i forgot to do and do after break

--- products.py
+++ products.py
@@ -4,11 +4,11 @@ class Products:
             raise ValueError('name can not be empty')
         else: self.name = name
 
-        if price > 0 and isinstance(price, float):
+        if price > 0:
             self.price = float(price)
         else:
             raise ValueError('price can not be negative')
-        if quantity > 0 and isinstance(quantity, int):
+        if quantity > 0:
             self.quantity = quantity
         else:
             raise ValueError('quantity must be > 0')
@@ -24,7 +24,7 @@ class Products:
             return True
         else:
             raise ValueError('quantity must be > 0')
-
+    # missing auto deactivate
 
     def is_active(self) -> bool:
         return self.active
@@ -43,10 +43,10 @@ class Products:
 
 
     def buy(self, quantity: int) -> float:
-
-        actual_q = get_quantity()
+# missing the thing.. like can we buy more than in stock etc.
+        actual_q = self.get_quantity()
         new_q = actual_q - quantity
         if new_q > 0:
-            set_quantity(new_q)
+            self.set_quantity(new_q)
 
         return quantity * self.price
