014f8093b328fa7b8a9b0edbf27f3c0b096c941a
Dennis Budek <dennis.budek@protonmail.com>
2026-07-16 11:32:14 +0200

    added bit of exception handling and logic

--- products.py
+++ products.py
@@ -1,49 +1,61 @@
 class Products:
     def __init__(self, name: str, price: float, quantity: int) -> None:
         if not name.strip():
-            raise ValueError('name can not be empty')
-        else: self.name = name
+            raise ValueError("name can not be empty")
+        else:
+            self.name = name.strip()
 
-        if price > 0:
-            self.price = float(price)
-        else:
-            raise ValueError('price can not be negative')
-        if quantity > 0:
-            self.quantity = quantity
-        else:
-            raise ValueError('quantity must be > 0')
+        try:
+            price = float(price)
+            if price > 0:
+                self.price = price
+            else:
+                raise ValueError("price can not be negative")
+        except Exception as e:
+            raise ValueError(f"Price must be a number.")
+        try:
+            quantity = int(quantity)
+            if quantity > 0:
+                self.quantity = quantity
+            else:
+                raise ValueError("quantity must be > 0")
+        except ValueError as e:
+            raise ValueError("Quantity must be a number!")
         self.active = True
 
     def get_quantity(self) -> int:
-            return self.quantity
-
+        return self.quantity
 
     def set_quantity(self, quantity: int):
-        if quantity > 0 and isinstance(quantity, int):
-            self.quantity = quantity
-            return True
+        if not isinstance(quantity, int):
+            raise ValueError("quantity must be a whole number (integer).")
+
         else:
-            raise ValueError('quantity must be > 0')
-    # missing auto deactivate
+            quantity = int(quantity)
+
+            if quantity < 0:
+                raise ValueError("quantity must be > 0")
+            elif quantity == 0:
+                self.deactivate()
+                return False
+            else:
+                self.quantity = quantity
+                return True
 
     def is_active(self) -> bool:
         return self.active
 
-
     def activate(self):
         self.active = True
-
 
     def deactivate(self):
         self.active = False
 
-
     def show(self):
         print(f"{self.name}, Price: {self.price}, Quantity: {self.quantity}")
 
-
     def buy(self, quantity: int) -> float:
-# missing the thing.. like can we buy more than in stock etc.
+        # missing the thing.. like can we buy more than in stock etc.
         actual_q = self.get_quantity()
         new_q = actual_q - quantity
         if new_q > 0:
