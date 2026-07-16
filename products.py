b121a6732f53f52a00b723b0a3d27d324c37dd46
Dennis Budek <dennis.budek@protonmail.com>
2026-07-16 11:53:35 +0200

    added bit of exception handling and logic II

--- products.py
+++ products.py
@@ -1,27 +1,23 @@
-class Products:
+class Product:
     def __init__(self, name: str, price: float, quantity: int) -> None:
         if not name.strip():
             raise ValueError("name can not be empty")
         else:
             self.name = name.strip()
-
         try:
             price = float(price)
-            if price > 0:
-                self.price = price
-            else:
-                raise ValueError("price can not be negative")
         except Exception as e:
             raise ValueError(f"Price must be a number.")
+        if price > 0:
+            self.price = price
+        else:
+            raise ValueError("price can not be negative")
         try:
             quantity = int(quantity)
-            if quantity > 0:
-                self.quantity = quantity
-            else:
-                raise ValueError("quantity must be > 0")
         except ValueError as e:
             raise ValueError("Quantity must be a number!")
-        self.active = True
+        self.activate()
+        self.set_quantity(quantity)
 
     def get_quantity(self) -> int:
         return self.quantity
@@ -34,8 +30,9 @@ class Products:
             quantity = int(quantity)
 
             if quantity < 0:
-                raise ValueError("quantity must be > 0")
+                raise ValueError("quantity must be >= 0")
             elif quantity == 0:
+                self.quantity = 0
                 self.deactivate()
                 return False
             else:
@@ -55,10 +52,13 @@ class Products:
         print(f"{self.name}, Price: {self.price}, Quantity: {self.quantity}")
 
     def buy(self, quantity: int) -> float:
-        # missing the thing.. like can we buy more than in stock etc.
-        actual_q = self.get_quantity()
-        new_q = actual_q - quantity
-        if new_q > 0:
-            self.set_quantity(new_q)
+
+        if quantity <= 0:
+            raise ValueError("Quantity must be positive")
+
+        if quantity > self.get_quantity():
+            raise ValueError("Not enough stock")
+
+        self.set_quantity(self.get_quantity() - quantity)
 
         return quantity * self.price
