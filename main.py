e121b0f8c98784302517a10d49a098f1ba46e6d8
Dennis Budek <dennis.budek@protonmail.com>
2026-07-16 12:30:25 +0200

    check if working

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from products import Product
+from store import Store
 
 bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
 mac = Product("MacBook Air M2", price=1450, quantity=100)
@@ -14,13 +15,13 @@ bose.set_quantity(1000)
 bose.show()
 
 
-product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
-                products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-                products.Product("Google Pixel 7", price=500, quantity=250),
+product_list = [Product("MacBook Air M2", price=1450, quantity=100),
+                Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                Product("Google Pixel 7", price=500, quantity=250),
                ]
 
 best_buy = Store(product_list)
 products = best_buy.get_all_products()
 print(best_buy.get_total_quantity())
 print(best_buy.order([(products[0], 1), (products[1], 2)]))
-Put it in your main function in the same file, and make sure you are handling the importing in the right manner.
+# Put it in your main function in the same file, and make sure you are handling the importing in the right manner.
--- store.py
+++ store.py
@@ -3,7 +3,6 @@ from products import Product
 
 
 class Store:
-
     def __init__(self, products: List[Product]):
         self.products = products
 
