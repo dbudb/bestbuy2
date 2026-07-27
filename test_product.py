8f52bd3c2b551f4c6877f8870d98e7bf9c5be865
Dennis Budek <dennis.budek@protonmail.com>
2026-07-27 11:30:33 +0200

    added unit tests for product class

---
+++ test_product.py
@@ -0,0 +1,47 @@
+import pytest
+
+from products import Product
+
+
+def test_create_normal_product():
+    product = Product("Laptop", 999.99, 10)
+
+    assert product.name == "Laptop"
+    assert product.price == 999.99
+    assert product.get_quantity() == 10
+    assert product.is_active()
+
+
+def test_create_product_with_invalid_details():
+    with pytest.raises(ValueError):
+        Product("", 100, 10)
+
+    with pytest.raises(ValueError):
+        Product("Laptop", -1, 10)
+
+
+def test_product_becomes_inactive_at_zero_quantity():
+    product = Product("Laptop", 999.99, 10)
+
+    product.set_quantity(0)
+
+    assert product.get_quantity() == 0
+    assert not product.is_active()
+
+
+def test_buy_modifies_quantity_and_returns_total():
+    product = Product("Laptop", 100, 10)
+
+    total = product.buy(3)
+
+    assert total == 300
+    assert product.get_quantity() == 7
+
+
+def test_buy_more_than_available_raises_exception():
+    product = Product("Laptop", 100, 2)
+
+    with pytest.raises(ValueError):
+        product.buy(3)
+
+    assert product.get_quantity() == 2
